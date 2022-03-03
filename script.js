1bb34e249b1b432596bd135365780ccb0bc0f147
Piero <dolibiroli@gmail.com>
2022-03-03 10:33:23 -0600

    added basic structure to html and basic functions to script

---
+++ script.js
@@ -0,0 +1,17 @@
+function operate(x, y, operator) {
+    switch (operator) {
+        case '+':
+            return sum(x, y);
+        case '-':
+            return subtract(x, y);
+        case '/':
+            return divide(x, y);
+        case '*':
+            return multiply(x, y);
+    }
+}
+
+const sum = (x, y) => x + y;
+const subtract = (x,y) => x-y;
+const divide = (x,y) => x/y;
+const multiply = (x,y) => x*y;
