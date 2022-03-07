99af06afb042f605380f180568d2fa1519addbe8
Piero <dolibiroli@gmail.com>
2022-03-06 23:28:54 -0600

    simplified js

--- script.js
+++ script.js
@@ -1,38 +1,95 @@
-function operate(x, y, operator) {
-    switch (operator) {
-        case '+':
-            return sum(x, y);
-        case '-':
-            return subtract(x, y);
-        case '/':
-            return divide(x, y);
-        case '*':
-            return multiply(x, y);
+const display = document.querySelector('#display');
+
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equals');
+
+let firstNumber = 0;
+let operator = '';
+
+
+// numbers.forEach((number) => {
+//     number.addEventListener('click', () => {
+//         showNumbers(number, 'numbers');
+//     })
+// });
+
+// operators.forEach((symbol) => {
+//     symbol.addEventListener('click', () => {
+//         chooseOperator(symbol);
+//     })
+// });
+
+// equals.addEventListener('click', () => {
+//     operate(operator);
+// });
+
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.className) {
+            case ('number'):
+                
+                console.log(button.id);
+                break;
+            case ('operator'):
+                console.log(button.id)
+                break;
+            case ('equals'):
+                console.log(button.id)
+                break;
+            case ('clc'):
+                console.log(button.id)
+                break;
+        }
+    })
+});
+
+
+
+
+function showNumbers(number, cases) {
+    switch (cases) {
+        case 'numbers':
+            display.textContent += number.id;
+            break;
+        case 'equals':
+            display.textContent = number;
+            break;
+    }
+
+}
+
+function chooseOperator(symbol) {
+    firstNumber = parseInt(display.textContent);
+    display.textContent = '';
+    operator = symbol;
+
+}
+
+function operate(operator) {
+    let secondNumber = parseInt(display.textContent);
+
+    switch (operator.id) {
+        case 'plus':
+            showNumbers(sum(firstNumber, secondNumber), 'equals');
+            break;
+        case 'subtract':
+            showNumbers(subtract(firstNumber, secondNumber, 'equals'));
+            break;
+        case 'divide':
+            showNumbers(divide(firstNumber, secondNumber, 'equals'));
+            break;
+        case 'multiply':
+            showNumbers(multiply(firstNumber, secondNumber, 'equals'));
+            break;
     }
 }
 
 const sum = (x, y) => x + y;
-const subtract = (x,y) => x-y;
-const divide = (x,y) => x/y;
-const multiply = (x,y) => x*y;
-
-function showNumbers(number){
-
-    display.textContent += number;
-    displayNumber = display.textContent;
-    // return number;
-}
-
-const display = document.querySelector('#display');
-let displayNumber = 0;
-
-
-const numbers = document.querySelectorAll('.number');
-
-numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        showNumbers(number.id);
-    })
-});
-
+const subtract = (x, y) => x - y;
+const divide = (x, y) => x / y;
+const multiply = (x, y) => x * y;
 
